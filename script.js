4305997a9d9448660a31dea67a2a3c2b2e3fe9aa
noelhoppe <noel.hoppe@gmx.de>
2024-03-04 19:35:06 +0100

    initial commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
+    // Desktop
     /*
-    Toggle navbar dropdown list
+    Toggle navbar dropdown list icons
      */
     var linkDropdown = document.querySelectorAll(".link-dropdown");
     linkDropdown.forEach(function (element) {
@@ -8,5 +9,39 @@ document.addEventListener("DOMContentLoaded", function () {
             element.getAttribute("data-toggle") == "close" ? element.setAttribute("data-toggle", "open") : element.setAttribute("data-toggle", "close");
             console.log(element);
         });
+    });
+    /*
+    Toggle dropdown items visibility (features)
+     */
+    var featuresDropdpwn = document.getElementById("feature-dropdown");
+    var featuresDropdownItems = document.getElementById("feature-dropdown-items");
+    featuresDropdpwn.addEventListener("click", function () {
+        featuresDropdownItems.classList.toggle("d-none");
+    });
+    /*
+    Toggle dropdown items visibility (company)
+    */
+    var companyDropdown = document.getElementById("company-dropdown");
+    var companyDropdownItems = document.getElementById("company-dropdown-items");
+    companyDropdown.addEventListener("click", function () {
+        companyDropdownItems.classList.toggle("d-none");
+    });
+    // Mobile
+    /*
+    Toggle hamburger menu status
+     */
+    var hamburgerMenu = document.querySelector("#hamburger-menu");
+    hamburgerMenu.addEventListener("click", function () {
+        hamburgerMenu.getAttribute("data-toggle") == "opened" ? hamburgerMenu.setAttribute("data-toggle", "closed") : hamburgerMenu.setAttribute("data-toggle", "opened");
+    });
+    var featureDropdownMobile = document.querySelector("#feature-dropdown-mobile");
+    var featureDropdownMobileItems = document.querySelector("#feature-dropdown-items-mobile");
+    featureDropdownMobile.addEventListener("click", function () {
+        featureDropdownMobileItems.classList.toggle("d-none");
+    });
+    var companyDropdownMobile = document.querySelector("#company-dropdown-mobile");
+    var companyDropdownMobileItems = document.querySelector("#company-dropdown-items-mobile");
+    companyDropdownMobile.addEventListener("click", function () {
+        companyDropdownMobileItems.classList.toggle("d-none");
     });
 });
--- script.ts
+++ script.ts
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
+    // Desktop
     /*
-    Toggle navbar dropdown list
+    Toggle navbar dropdown list icons
      */
     const linkDropdown = document.querySelectorAll(".link-dropdown") as NodeListOf<HTMLAnchorElement>;
     linkDropdown.forEach((element : HTMLAnchorElement) => {
@@ -8,5 +9,44 @@ document.addEventListener("DOMContentLoaded", () => {
             element.getAttribute("data-toggle") == "close" ? element.setAttribute("data-toggle", "open") : element.setAttribute("data-toggle", "close");
             console.log(element);
         })
+    })
+
+    /*
+    Toggle dropdown items visibility (features)
+     */
+    const featuresDropdpwn : HTMLAnchorElement = document.getElementById("feature-dropdown") as HTMLAnchorElement;
+    const featuresDropdownItems : HTMLAnchorElement = document.getElementById("feature-dropdown-items") as HTMLAnchorElement;
+    featuresDropdpwn.addEventListener("click", () => {
+        featuresDropdownItems.classList.toggle("d-none");
+    })
+
+    /*
+    Toggle dropdown items visibility (company)
+    */
+    const companyDropdown : HTMLAnchorElement = document.getElementById("company-dropdown") as HTMLAnchorElement;
+    const companyDropdownItems : HTMLAnchorElement = document.getElementById("company-dropdown-items") as HTMLAnchorElement;
+    companyDropdown.addEventListener("click", () => {
+        companyDropdownItems.classList.toggle("d-none");
+    })
+
+    // Mobile
+    /*
+    Toggle hamburger menu status
+     */
+    const hamburgerMenu:HTMLButtonElement = document.querySelector("#hamburger-menu") as HTMLButtonElement;
+    hamburgerMenu.addEventListener("click", () => {
+        hamburgerMenu.getAttribute("data-toggle") == "opened" ? hamburgerMenu.setAttribute("data-toggle", "closed"): hamburgerMenu.setAttribute("data-toggle", "opened");
+    })
+
+    const featureDropdownMobile : HTMLButtonElement = document.querySelector("#feature-dropdown-mobile") as HTMLButtonElement;
+    const featureDropdownMobileItems: HTMLElement = document.querySelector("#feature-dropdown-items-mobile")  as HTMLElement;
+    featureDropdownMobile.addEventListener("click", () => {
+        featureDropdownMobileItems.classList.toggle("d-none");
+    })
+
+    const companyDropdownMobile : HTMLButtonElement = document.querySelector("#company-dropdown-mobile") as HTMLButtonElement;
+    const companyDropdownMobileItems: HTMLElement = document.querySelector("#company-dropdown-items-mobile") as HTMLElement;
+    companyDropdownMobile.addEventListener("click", () => {
+        companyDropdownMobileItems.classList.toggle("d-none");
     })
 })
